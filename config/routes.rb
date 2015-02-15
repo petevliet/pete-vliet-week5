a3449332b39368ab758c89d4e13feecbf052e2b6
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-02-15 15:40:49 -0700

    user can sign out, index shows diff things when signed in/out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'locations#index'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 
   resources :locations do
     resources :events
